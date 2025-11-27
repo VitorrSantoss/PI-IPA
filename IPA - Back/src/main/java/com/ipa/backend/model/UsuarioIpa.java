830501e6a8de9753115aaca650fdb3f2c2c2e468
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-11-26 22:05:55 -0300

    renomeação da tabela entidade no banco de dados

---
+++ IPA - Back/src/main/java/com/ipa/backend/model/UsuarioIpa.java
@@ -0,0 +1,40 @@
+package com.ipa.backend.model;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_usuarios_ipa")
+public class UsuarioIpa {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 150)
+  private String nome;
+
+  @CPF
+  @Column
+  private String cpf;
+
+  @Column(length = 20)
+  private String telefone;
+
+  @Column(length = 9)
+  private String matriculaIpa;
+  
+  @Column
+  private String localAtuacao;
+
+}
