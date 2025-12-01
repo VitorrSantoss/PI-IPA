30fb70212306cdf4c969eab4d54947c511f1ba11
Vitor <vitorvitorsantos2015@hotmail.com>
2025-12-01 14:03:51 -0300

    controller atualizado: nome e estrutura

--- IPA - Back/src/main/java/com/ipa/backend/controller/UsuarioController.java
+++ IPA - Back/src/main/java/com/ipa/backend/controller/UsuarioController.java
@@ -1,13 +1,21 @@
 package com.ipa.backend.controller;
 
-import com.ipa.backend.dto.UsuarioDTO;
-import com.ipa.backend.service.UsuarioService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.ipa.backend.dto.UsuarioDTO;
+import com.ipa.backend.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api/usuarios")
